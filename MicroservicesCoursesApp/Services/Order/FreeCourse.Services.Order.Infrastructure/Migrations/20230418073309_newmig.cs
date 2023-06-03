44a17894dc0c885c14cdd8285de5d4e89deb3047
Furkan Berk Yılmazer <furkanberkyilmazer@users.noreply.github.com>
2023-06-03 14:33:57 +0300

    Microsevices_CoursesApp

---
+++ MicroservicesCoursesApp/Services/Order/FreeCourse.Services.Order.Infrastructure/Migrations/20230418073309_newmig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FreeCourse.Services.Order.Infrastructure.Migrations
+{
+    public partial class newmig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
